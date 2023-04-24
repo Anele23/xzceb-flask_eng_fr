6f50807661a2aacabc546f424e466da5e4bf1fe7
anele23 <anele.kweyama@gmail.com>
2023-04-24 07:34:02 -0400

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,25 @@
+"""Translator Test Script."""
+import unittest
+from translator import english_to_french, french_to_english
+
+class TestEnglishToFrench(unittest.TestCase):
+    """
+    Class that contains tests methods for English to French.
+    """
+    def test1(self):
+        """
+        Method to test English to French.
+        """
+        self.assertNotEqual(english_to_french("Hello"),None)
+        self.assertEqual(english_to_french("Hello"),'Bonjour')
+
+class TestFrenchToEnglish(unittest.TestCase):
+    """
+    Class that contains tests methods for French to English.
+    """
+    def test1(self):
+        """
+        Method to test French to English.
+        """
+        self.assertNotEqual(french_to_english("Bonjour"),None)
+        self.assertEqual(french_to_english("Bonjour"),'Hello')
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,45 @@
+"""Translator Script."""
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+translator.set_service_url(url)
+
+def english_to_french(english_text):
+    """
+        Translate Engilsh to Franch.
+    """
+    translation = translator.translate(
+        text=english_text,
+        source='en',
+        target='fr'
+    ).get_result()
+
+    french_text = translation['translations'][0]['translation']
+
+    return french_text
+
+def french_to_english(french_text):
+    """
+        Translate Franch to Engilsh
+    """
+    translation = translator.translate(
+        text=french_text,
+        source='fr',
+        target='en'
+    ).get_result()
+
+    english_text = translation['translations'][0]['translation']
+    return english_text
